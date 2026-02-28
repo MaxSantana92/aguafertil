bb66418c259667ef7add02d68428606058afe82f
Max Santana <maxsantana.dev@gmail.com>
2026-02-28 18:29:47 -0300

    Update routing configuration in astro.config.mjs: add redirectToDefaultLocale option to improve language handling.

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,8 @@ export default defineConfig({
     defaultLocale: 'es',
     locales: ['es', 'en'],
     routing: {
-        prefixDefaultLocale: true
+        prefixDefaultLocale: true,
+        redirectToDefaultLocale: false
     }
   },
   vite: {
